b369c02d772c3390981b76f3b6050a92fc08e06c
premvir.chaudhary@gmail.com <premvir.chaudhary@gmail.com>
2022-02-27 23:35:20 +0530

    added maps and cleanup

--- AirlineTicketManagement.sol
+++ AirlineTicketManagement.sol
@@ -24,38 +24,35 @@ contract AirlineTicketManagement {
     */
     enum FlightStatus {Scheduled, BookingOpen, BookingClosed, Delayed, Departed, Landed, Cancelled}
 
-    /**
-        1. Seat is available but not open for booking - Initial
-        2. Seat is available for booking - Free
-        3. Seat is under booking  - Blocked
-        4. Seat is allocated to customer - Booked 
-    */
-    enum SeatStatus {Initialise, Free, Blocked, Booked}
-
-    /**
-        events
-    */
-    enum FlightEvents{FlightInitialisation, BookingOpen, BookingClosed, Delayed, Flying, Cancelled}
 
     /**
     Airline user details in struct
     */
     struct AirlineUser {
-      address     user;
+      address       user;
       string        userName;
-      string        userId;
     }
 
+
     /**
-     flight seat details 
+        booking status
     */
-    struct Seat {
-        uint256         seatId;
-        address     owner;
-        address     passenger;
-        uint      price;
-      SeatStatus      status;
+    enum BookingStatus {Refunded, Booked, Cancelled}
+    /**
+        booking details
+    */
+    struct BookingDetails{
+        address         userAddress;
+        address         airlineAddress;
+        uint8           noOfTickets;
+        uint256         totalPerTicket;
+        BookingStatus   bookingStatus;
     }
+
+    //key for bookingMap will be userAddess + flightId + StartDate
+    mapping(string => BookingDetails) bookingMap;
+    //Key -> flightId
+    mapping(string => Flight) flightStatusMap;
 
     /**
         flight details
@@ -63,13 +60,12 @@ contract AirlineTicketManagement {
     struct Flight{
       string        flightId;
       string        airlineName;
-      uint256     seatCount; 
+      uint256       availableSeats; 
       string        src;
       string        dest;
-      uint256     departureTime;
-      uint256     reachingTime;
+      uint256       departureTime;
+      uint256       reachingTime;
       FlightStatus  status;
-      Seat        []seats;
     }
 
     /**
@@ -78,8 +74,6 @@ contract AirlineTicketManagement {
     struct Traveller{
       address   traveller;
       string      name;
-      string      age;
-      string      govtId;
     }
 
     //temp to remove
@@ -88,32 +82,24 @@ contract AirlineTicketManagement {
     mapping (address => AirlineUser) airlineUsers;
     mapping (address => Traveller) travellerUsers;
     Flight []       public flights;
-    uint256         flightCounter;
 
     FlightStatus    flightStatus;
     SeatStatus      seatStatus;
     FlightEvents    flightEvent;
 
 
-    constructor(uint256 seatCount, uint256 flightCount) {
+    constructor() {
 
         // airline users
-        
         airlineUsers[msg.sender] = AirlineUser(msg.sender, 'AirlineUser1', 'AU01');
-
         airlineUsers[msg.sender] = AirlineUser(msg.sender, 'AirlineUser2', 'AU02');
 
         //traveller users
-        travellerUsers[msg.sender] = Traveller(msg.sender, '24', 'Bharat', 'UAID-01');
-        travellerUsers[msg.sender] = Traveller(msg.sender, '25', 'Shyam', 'UAID-02');
-        travellerUsers[msg.sender] = Traveller(msg.sender, '27', 'Mohan', 'UAID-03');
-        travellerUsers[msg.sender] = Traveller(msg.sender, '34', 'Abhi', 'UAID-04');
-        travellerUsers[msg.sender] = Traveller(msg.sender, '38', 'Akash', 'UAID-05');
-
-        numberOfSeats =  seatCount;
-        numberOfFlights = flightCount;
-        flightCounter = 0;
-
+        travellerUsers[msg.sender] = Traveller(msg.sender, 'Bharat');
+        travellerUsers[msg.sender] = Traveller(msg.sender, 'Shyam');
+        travellerUsers[msg.sender] = Traveller(msg.sender, 'Mohan');
+        travellerUsers[msg.sender] = Traveller(msg.sender, 'Abhi');
+        travellerUsers[msg.sender] = Traveller(msg.sender, 'Akash');
     }
 
     //only airline users will be allowed
@@ -126,24 +112,5 @@ contract AirlineTicketManagement {
     modifier isTravellerUser() {
         require(msg.sender == travellerUsers[msg.sender].traveller, "Caller is not traveller");
         _;
-    }
-
-    //method to add flight
-    function addFlight(string memory flightId, string memory flightName, string memory src, string memory dest, uint256 depTime, 
-                        uint256 landingTime, uint256 price) public isAirlineUser {
-
-        
-        Flight memory flight = Flight(flightId, flightName, numberOfSeats, src, dest, depTime, landingTime, FlightStatus.Scheduled, addSeats(price));
-        flights.push(flight);
-        flightCounter += 1;
-    }
-
-    // function to initialise seats 
-    function addSeats(uint256 price) public view returns(Seat[] memory seatList){
-        for (uint256 i = 1; i <= numberOfSeats; i ++){
-            Seat memory seat = Seat(i, address(0), address(0), price, SeatStatus.Initialise);
-            seatList[i-1] = seat;
-        }
-        return seatList;
     }
 }
